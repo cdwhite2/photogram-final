a63c2825c5e8d52f81a1556648a67c7c9c673bf4
cdwhite2 <cdwhite@protonmail.com>
2024-11-20 22:52:26 +0000

    public photos page does not show photos from private accounts

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,7 +12,7 @@
 #  owner_id       :integer
 #
 class Photo < ApplicationRecord
-  belongs_to(:poster, class_name: "User", foreign_key: "user_id")
+  belongs_to(:poster, class_name: "User", foreign_key: "owner_id")
   has_many(:comments, class_name: "Comment", foreign_key: "photo_id")
   has_many(:likes, class_name: "Like", foreign_key: "photo_id")
   has_many(:fans, through: :likes, source: :fan)
